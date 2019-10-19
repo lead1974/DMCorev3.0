6951020509b3a9d89ce594f042de5aa6fdf98963
lead1974 <email2lead@gmail.com>
2019-10-18 17:07:54 -0700

    10.18.19 Admin index page design

--- DMCore/wwwroot/js/main.js
+++ DMCore/wwwroot/js/main.js
@@ -1,40 +1,57 @@
 ﻿jQuery(function ($) {
 
     //#main-slider
-    $(function () {
-        $('#main-slider.carousel').carousel({
-            interval: 8000
-        });
-    });
+    //$(function () {
+    //    $('#main-slider.carousel').carousel({
+    //        interval: 8000
+    //    });
+    //});
 
-    $('.centered').each(function (e) {
-        $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
-    });
+    //$('.centered').each(function (e) {
+    //    $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+    //});
 
-    $(window).resize(function () {
-        $('.centered').each(function (e) {
-            $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
-        });
-    });
+    //$(window).resize(function () {
+    //    $('.centered').each(function (e) {
+    //        $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+    //    });
+    //});
 
-    //portfolio
-    $(window).on('load', function () {
-        $portfolio_selectors = $('.portfolio-filter >li>a');
-        if ($portfolio_selectors != 'undefined') {
-            $portfolio = $('.portfolio-items');
-            $portfolio.isotope({
-                itemSelector: 'li',
-                layoutMode: 'fitRows'
-            });
-            $portfolio_selectors.on('click', function () {
-                $portfolio_selectors.removeClass('active');
-                $(this).addClass('active');
-                var selector = $(this).attr('data-filter');
-                $portfolio.isotope({ filter: selector });
-                return false;
-            });
-        }
-    });
+    ////portfolio
+    //$(window).on('load', function () {
+    //    $portfolio_selectors = $('.portfolio-filter >li>a');
+    //    if ($portfolio_selectors != 'undefined') {
+    //        $portfolio = $('.portfolio-items');
+    //        $portfolio.isotope({
+    //            itemSelector: 'li',
+    //            layoutMode: 'fitRows'
+    //        });
+    //        $portfolio_selectors.on('click', function () {
+    //            $portfolio_selectors.removeClass('active');
+    //            $(this).addClass('active');
+    //            var selector = $(this).attr('data-filter');
+    //            $portfolio.isotope({ filter: selector });
+    //            return false;
+    //        });
+    //    }
+    //});
+    //$(window).on('load', function () {
+    //    $portfolio_selectors = $('.portfolio-filter >li>a');
+    //    if ($portfolio_selectors != 'undefined') {
+    //        $portfolio = $('.portfolio-items');
+    //        $portfolio.isotope({
+    //            itemSelector: 'li',
+    //            layoutMode: 'fitRows'
+    //        });
+    //        $portfolio_selectors.on('click', function () {
+    //            $portfolio_selectors.removeClass('active');
+    //            $(this).addClass('active');
+    //            var selector = $(this).attr('data-filter');
+    //            $portfolio.isotope({ filter: selector });
+    //            return false;
+    //        });
+    //    }
+    //});
 
     //contact form
     var form = $('.contact-form');
@@ -57,5 +74,31 @@
     //Pretty Photo
     $("a[rel^='prettyPhoto']").prettyPhoto({
         social_tools: false
+    });
+
+    $(window).on('load', function () {
+        var $grid = $('.grid').isotope({
+            // options
+        });
+        $('.filter-button-group').on('click', 'button', function () {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({ filter: filterValue });
+            return false;
+        });
+
+        // bind sort button click
+        $('#sorts').on('click', 'button', function () {
+        var sortByValue = $(this).attr('data-sort-by');
+        $grid.isotope({ sortBy: sortByValue });
+        });
+
+        // change is-checked class on buttons
+        $('.button-group').each(function (i, buttonGroup) {
+            var $buttonGroup = $(buttonGroup);
+            $buttonGroup.on('click', 'button', function () {
+                $buttonGroup.find('.active').removeClass('active');
+                $(this).addClass('active');
+            });
+        });
     });
 });
